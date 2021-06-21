eb480e9ff0ad07255a0101357d46f776bdc3ee8b
DC07OCT <76606636+DC07OCT@users.noreply.github.com>
2021-06-21 18:10:09 +0800

    Update DSAI_FP_PredFuture_test.py

--- DSAI_FP_PredFuture_test.py
+++ DSAI_FP_PredFuture_test.py
@@ -8,10 +8,7 @@ train_df=pd.read_csv(path+'/competitive-data-science-predict-future-sales/sales_
 #Test 值
 test_input = data_train[data_train.date_block_num == 34].drop(['item_cnt_month'], axis=1)
 
-# #更新特徵值'item_price_mean'刪掉
-# group = train_df.groupby(['shop_id','item_id'])['item_price'].mean().rename('item_price_mean').reset_index()
-# test_input = pd.merge(test_input.drop(['item_price_mean'], axis=1), group, on=['shop_id','item_id'], how='left').fillna(0)
-# print(test_input)
+
 
 from xgboost import XGBRegressor
 #設定模型參數
